9889e5232ce643668bb96b2f428e6436de137b05
Sudipta Dey <sudupta152@gmail.com>
2026-03-23 21:25:36 -0400

    fixing style issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+const { purge } = require('tailwindcss/stubs/defaultConfig.stub');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   darkMode: 'class', // optional for dark mode
   plugins: [],
+  purge: false,
 };
